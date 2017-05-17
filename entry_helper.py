12753f9ee054409e26f3f89f136d732934171863
acmeow <ac@anqic.com>
2017-05-17 01:33:04 -0500

    remove 'format' option, add 'reset' option

--- entry_helper.py
+++ entry_helper.py
@@ -1,14 +1,15 @@
+import sys
 def main():
     entry_num = int(raw_input("Enter session count: "))
     while True:
-        print "------- MESSAGE: enter summoner name or 'quit' to exit program -------"
+        print "\n------- MESSAGE: enter summoner name or 'quit' to exit program -------"
         username = raw_input("Enter: ")
         if username == "quit":
             return
 
         if check_usernames(username) is False:
             while True:
-                print "------- MESSAGE: enter attributes or 'format' for format -------"
+                print "------- MESSAGE: enter attributes or 'reset' for reset summoner name -------"
                 entry = raw_input("Enter: ")
                 if entry == "reset":
                     break
@@ -27,25 +28,11 @@ def check_usernames(username):
     for line in f.readlines():
         if username == line.strip():
             found = True
-            print("------- ERROR: summoner found in database -------")
+            print "------- ERROR: summoner found in database -------"
             f.close()
     return found
 
 def add_entry(entry):
-    if entry == "format":
-        print("@attribute 'preferred position' {top,jungle,mid,adc,support}\n" +
-            "@attribute 'current rank(%of top)' numeric\n" +
-            "@attribute 'recent P/kill' numeric\n" +
-            "@attribute 'highest champ gold' numeric\n" +
-            "@attribute 'highest champ cs' numeric\n" +
-            "@attribute 'avg turrent' numeric\n" +
-            "@attribute 'avg dmg' numeric\n" +
-            "@attribute 'number of seasons' numeric\n" +
-            "@attribute 'number of recent teammates' numeric\n" +
-            "@attribute '# of games played with most played champion' numeric\n" +
-            "@attribute 'overall winrate' {high,medium,low}")
-        return False
-
     entry_list = entry.split(',')
     if len(entry_list) != 11:
         print "------- ERROR: length of attributes is not correct -------"
